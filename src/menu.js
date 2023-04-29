9482d2302a536e5fbdab71e462ccafd2f30a5044
balanggabriel09@gmail.com <balanggabriel09@gmail.com>
2023-04-29 15:08:16 +0800

    ""

--- src/menu.js
+++ src/menu.js
@@ -5,12 +5,35 @@ class MenuScene extends Phaser.Scene{
 //<---------- PRELOAD ---------->
   preload()
   {
-    
+    this.load.image('apple', 'assets/images/apple.png')
+    this.load.image('arrow', 'assets/images/arrow.png')
+    this.load.image('backyard', 'assets/images/backyard.png')
+    this.load.image('bar', 'assets/images/bar.png')
+    this.load.image('candy', 'assets/images/candy.png')
+    this.load.image('logo', 'assets/images/logo.png')
+    this.load.image('rotate', 'assets/images/rotate.png')
+    this.load.image('duck', 'assets/images/rubber_duck.png')
+    this.load.spritesheet('pet', 'assets/images/pet.png', {
+      frameWidth: 97, frameHeight: 83
+    })
   }
 //<---------- CREATE  ---------->
   create()
   {
-    console.log('MENU')
-    this.scene.start('Scene1')
- }
+    this.background = this.add.image(0, 0, 'backyard').setOrigin(0, 0);
+    this.background.setInteractive()
+    this.background.on('pointerdown', function()
+    {
+      this.scene.start('Scene1')
+    }, this);
+    
+    this.instruct = this.add.text(game.scale.width/2, (game.scale.height/2) + (game.scale.height/4), 'CLICK TO START').setOrigin(0.5)
+    this.instruct.setStyle({
+      fontFamily: 'Arial',
+      fontSize: 40,
+      color: '#000000',
+      stroke: '#000000',
+      strokeThickness: 1.6
+    })
+  }
 }
--- src/scene1.js
+++ src/scene1.js
@@ -5,5 +5,37 @@ class Scene1 extends Phaser.Scene {
   
   create(){
     console.log('SCENE1')
+//<---------- BACKGROUND IMAGE ---------->
+    this.background = this.add.image(0, 0, 'backyard').setOrigin(0, 0)
+    
+//<---------- BUTTONS ---------->
+    this.apple = this.add.sprite(72, 570, 'apple')
+    this.candy = this.add.sprite(144, 570, 'candy')
+    this.toy = this.add.sprite(216, 570, 'duck')
+    this.rotate = this.add.sprite(288, 570, 'rotate')
+
+//<---------- INTERACTIVE BUTTONS ---------->
+    this.apple.setInteractive({pixelPerfect: true, useHandCursor: true})
+    this.candy.setInteractive({pixelPerfect: true, useHandCursor: true})
+    this.toy.setInteractive({pixelPerfect: true, useHandCursor: true})
+    this.rotate.setInteractive({pixelPerfect: true, useHandCursor: true})
+
+//<---------- CUSTOM PARAMETERS ---------->
+    this.apple.instanceVariable = {health: 20}
+    this.candy.instanceVariable = {health: -10, fun: 10}
+    this.toy.instanceVariable = {fun: 20}
+
+//<---------- THE PET ---------->
+    this.pet = this.add.sprite(150, 300, 'pet')
+    this.pet.instanceVariable = {health: 100, fun: 100}
+    this.pet.setInteractive({pixelPerfect: true, useHandCursor: true})   
+    this.input.setDraggable(this.pet);
+    this.input.on('drag', (pointer, gameObject, dragX, dragY) =>
+    {
+      gameObject.x = dragX
+      gameObject.y = dragY
+    });
   }
+  
+  
 }
